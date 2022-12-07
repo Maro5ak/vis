37a9c0356173bba33f5d00c54fcfccd234281816
Maro5ak <marosak68.2002@gmail.com>
2022-12-07 15:15:58 +0100

    added img to instruments

--- PresentationLayer/InstrumentDetails.Designer.cs
+++ PresentationLayer/InstrumentDetails.Designer.cs
@@ -137,9 +137,10 @@
             // pictureBox1
             // 
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(167, 92);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(232, 235);
+            this.pictureBox1.Size = new System.Drawing.Size(235, 235);
             this.pictureBox1.TabIndex = 14;
             this.pictureBox1.TabStop = false;
             // 
